b16721e14603056816e7736996c064758deb3a4a
hopeurl@shiping.song <2453332538@qq.com>
2022-09-21 17:51:44 +0800

    [新增功能](master): 初始化仓库

---
+++ spring-boot-tutorial/websocket-template/src/main/java/com/gitee/xiaosongstudy/WebSocketTemplateApplication.java
@@ -0,0 +1,18 @@
+package com.gitee.xiaosongstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * websocket案例核心启动类 .<br>
+ *
+ * @author shiping.song
+ * @date 2022/9/21 17:31
+ */
+@SpringBootApplication
+public class WebSocketTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketTemplateApplication.class,args);
+    }
+}
