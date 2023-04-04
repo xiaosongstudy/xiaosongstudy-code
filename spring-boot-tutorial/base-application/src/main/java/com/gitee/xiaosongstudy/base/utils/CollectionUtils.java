f8d002a157a2022de01021871658df1e647233ab
hopuerl@shiping.song <2453332538@qq.com>
2023-04-04 20:03:07 +0800

    初始化腾讯云OCR识别模板项目

--- spring-boot-tutorial/base-application/src/main/java/com/gitee/xiaosongstudy/base/utils/CollectionUtils.java
+++
@@ -1,8 +0,0 @@
-package com.gitee.xiaosongstudy.base.utils;/**
- * 这是类注释 .<br>
- *
- * @author shiping.song
- * @date 2022/9/23 13:04
- */
-public class CollectionUtils {
-}
---
+++ spring-boot-tutorial/tencent-ocr-template/src/main/java/com/gitee/xiaosongstudy/tencent/TencentOcrTemplateApplication.java
@@ -0,0 +1,18 @@
+package com.gitee.xiaosongstudy.tencent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 腾讯云ocr操作模板启动类.<br>
+ *
+ * @author shiping.song
+ * @date 2023/4/4 19:58
+ */
+@SpringBootApplication
+public class TencentOcrTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TencentOcrTemplateApplication.class, args);
+    }
+}
